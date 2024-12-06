e802730651bb14cf2199e8e235b1860897c109be
ahasan2912 <habib15-4284@diu.edu.bd>
2024-12-06 16:35:28 +0600

    added Loading Componet and firebase authtication

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Loading = () => {
     return (
         <div>
-            
+            <span className="loading loading-bars loading-xs"></span>
+            <span className="loading loading-bars loading-sm"></span>
+            <span className="loading loading-bars loading-md"></span>
+            <span className="loading loading-bars loading-lg"></span>
         </div>
     );
 };
